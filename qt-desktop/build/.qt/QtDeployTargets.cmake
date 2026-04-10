33c34c1a8d99d44bb70965b9b9a7508e802743f0
João Pedro <joao.borges2001@outlook.com>
2026-04-10 18:42:58 -0300

    project working with classes

--- qt-desktop/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_atsc_calculator_FILE /home/joao-pedro/Projetos/Mestrado/atsc-calculator/qt-desktop/build/atsc_calculator)
-set(__QT_DEPLOY_TARGET_atsc_calculator_TYPE EXECUTABLE)
--- qt-desktop/build/CMakeFiles/atsc_calculator_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for atsc_calculator_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
